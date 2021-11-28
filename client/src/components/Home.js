767556754c1e758be2d40a327c9d412075384678
tyler <tyler_1701@hotmail.com>
2021-11-28 16:40:08 -0500

    popular posts

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,15 +1,25 @@
 import * as React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
 
 import Trending from './Trending';
+import Popular from './Popular';
 
 export default function Home() {
+  const flexColumnStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+  };
+
   return (
     <React.Fragment>
       <CssBaseline />
       <Container maxWidth='lg'>
-        <Trending />
+        <Box sx={flexColumnStyle}>
+          <Trending />
+          <Popular />
+        </Box>
       </Container>
     </React.Fragment>
   );
