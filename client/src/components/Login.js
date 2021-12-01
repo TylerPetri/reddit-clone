998058db4d5904000459d2cb85d2286154fca9be
tyler <tyler_1701@hotmail.com>
2021-11-30 21:45:35 -0500

    login screen

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,8 +1,15 @@
 import * as React from 'react';
+import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
+import Divider from '@mui/material/Divider';
+import TextField from '@mui/material/TextField';
+
+import CloseIcon from '@mui/icons-material/Close';
+import GoogleIcon from '@mui/icons-material/Google';
+import AppleIcon from '@mui/icons-material/Apple';
 
 const loginStyle = {
   display: { xs: 'none', md: 'block' },
@@ -23,12 +30,61 @@ const modalStyle = {
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  width: 400,
+  height: '70%',
+  width: '85%',
   bgcolor: 'background.paper',
   border: '2px solid #000',
   boxShadow: 24,
-  p: 4,
+  display: 'flex',
 };
+
+const Img = styled(Box)({
+  height: '100%',
+  width: '100px',
+  backgroundColor: 'skyblue',
+});
+
+const ContentBox = styled(Box)({
+  height: '100%',
+  width: '100%',
+  display: 'flex',
+  flexDirection: 'column',
+});
+
+const ModalHeaderBox = styled(Box)({
+  display: 'flex',
+  flexDirection: 'column',
+  paddingLeft: 20,
+});
+
+const ContinueBox = styled(Box)({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'flex-start',
+  gap: 10,
+  marginTop: '50px',
+  paddingLeft: '20px',
+});
+
+const continueStyle = {
+  display: 'flex',
+  justifyContent: 'space-between',
+  width: '300px',
+  color: '#808080',
+  border: '1px solid grey',
+};
+
+const OrDividerBox = styled(Box)({
+  width: '320px',
+  color: '#808080',
+  marginTop: '40px',
+  paddingLeft: '20px',
+});
+
+const LoginButton = styled(Button)({
+  color: 'white',
+  background: '#007BD7',
+});
 
 export default function Login() {
   const [open, setOpen] = React.useState(false);
@@ -43,19 +99,78 @@ export default function Login() {
       <Button onClick={handleOpen} sx={loginSignupStyle}>
         Login / Signup
       </Button>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby='modal-modal-title'
-        aria-describedby='modal-modal-description'
-      >
+      <Modal open={open} onClose={handleClose}>
         <Box sx={modalStyle}>
-          <Typography id='modal-modal-title' variant='h6' component='h2'>
-            Text in a modal
-          </Typography>
-          <Typography id='modal-modal-description' sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
+          <Img />
+          <ContentBox>
+            <Box
+              sx={{
+                display: 'flex',
+                justifyContent: 'flex-end',
+                p: 2,
+                color: '#808080',
+              }}
+            >
+              <div className='close-button'>
+                <CloseIcon />
+              </div>
+            </Box>
+            <ModalHeaderBox>
+              <Typography variant='h6'>Login</Typography>
+              <Typography variant='caption'>
+                By continuing, you agree to our User Agreement and Privacy
+                Policy.
+              </Typography>
+            </ModalHeaderBox>
+            <ContinueBox>
+              <Button sx={continueStyle}>
+                <GoogleIcon />
+                <span style={{ margin: '0 auto' }}>Continue with Google</span>
+              </Button>
+              <Button sx={continueStyle}>
+                <AppleIcon />{' '}
+                <span style={{ margin: '0 auto' }}>Continue with Apple</span>
+              </Button>
+            </ContinueBox>
+            <OrDividerBox>
+              <Divider>OR</Divider>
+            </OrDividerBox>
+            <Box
+              component='form'
+              sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                marginTop: '20px',
+                paddingLeft: '20px',
+                width: '320px',
+                gap: 1,
+                '& .MuiTextField-root': { p: 0 },
+              }}
+              noValidate
+              autoComplete='off'
+            >
+              <TextField
+                fullWidth
+                id='outlined-username'
+                label='Username'
+                variant='outlined'
+              />
+              <TextField
+                id='outlined-password-input'
+                label='Password'
+                type='password'
+                autoComplete='current-password'
+                fullWidth
+              />
+              <LoginButton fullWidth>Log In</LoginButton>
+              <Typography variant='caption' color='text.secondary'>
+                Forgot your username of password?
+              </Typography>
+              <Typography variant='caption' color='text.secondary' mt={2}>
+                New to reddit? <Button>SIGN UP</Button>
+              </Typography>
+            </Box>
+          </ContentBox>
         </Box>
       </Modal>
     </div>
